7a91c0cf48d48932efe03b5f17f8e4552c641ecb
korali <korali1995@gmail.com>
2022-11-24 22:13:34 +0200

    userService

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,28 @@
-DROP TABLE IF EXISTS customer_order;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS poll;
+DROP TABLE IF EXISTS user_id;
 
-CREATE TABLE customer (
-    id int(11) unsigned NOT NULL AUTO_INCREMENT,
-    first_name varchar(300) NOT NULL DEFAULT '',
-    last_name varchar(300) NOT NULL DEFAULT '',
-    email varchar(300) NOT NULL DEFAULT '',
-    status varchar(300) NOT NULL DEFAULT 'REGULAR',
-    PRIMARY KEY (id)
+CREATE TABLE poll (
+        question_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+        question_title varchar(300) NOT NULL DEFAULT '',
+        first_answer_option varchar(300) NOT NULL DEFAULT '',
+        second_answer_option varchar(300) NOT NULL DEFAULT '',
+        third_answer_option varchar(300)  NULL DEFAULT '',
+        fourth_answer_option varchar(300) NOT NULL DEFAULT '',
+
+        PRIMARY KEY (question_id)
 );
 
-CREATE TABLE customer_order (
-    id int(11) unsigned NOT NULL AUTO_INCREMENT,
-    customer_id int(11) unsigned NOT NULL,
-    item_name varchar(300) NOT NULL DEFAULT '',
-    price int(11) NOT NULL DEFAULT '',
-    currency varchar(300) NOT NULL DEFAULT 'USD',
-    PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES customer(id)
-);
+CREATE TABLE user_id (
+    uniqueUser_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+    user_id int(11) unsigned NOT NULL ,
+    user_first_name varchar(300) unsigned NOT NULL,
+    user_last_name varchar(300) NOT NULL DEFAULT '',
+    user_email varchar (300) NOT NULL DEFAULT '',
+    user_age varchar (300) NOT NULL DEFAULT '',
+    user_address varchar (300) NOT NULL DEFAULT '',
+    user_date varchar (11) NOT NULL DEFAULT '',
 
-CREATE TABLE student (
-    id int(11) unsigned NOT NULL AUTO_INCREMENT,
-    first_name varchar(300) NOT NULL DEFAULT '',
-    last_name varchar(300) NOT NULL DEFAULT '',
-    email varchar(300) NOT NULL DEFAULT '',
-    PRIMARY KEY (id)
+
+    PRIMARY KEY (uniqueUser_id),
+    FOREIGN KEY (user_id) REFERENCES poll(question_id)
 );
