977f53edce7503b7559f299ca00d0e5f5dbb4ce6
Koral <korali1995@gmail.com>
2023-10-23 03:22:24 +0300

    FeignClients microService successfully

--- src/main/java/com/firstProject/pollService/PollServiceClient.java
+++ src/main/java/com/firstProject/pollService/PollServiceClient.java
@@ -12,5 +12,5 @@ import org.springframework.web.bind.annotation.*;
 public interface PollServiceClient {
 
     @DeleteMapping(value = "userAnswer/delete/{userAnswerId}")
-    void deleteUserAnswerById(@PathVariable Long userAnswerId);
+    void deleteUserAnswerById(@PathVariable Long id);
 }
